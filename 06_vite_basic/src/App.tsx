1f65ef239717a03abd289d4956d7ae4ede549bfa
soulmotion21 <sungkyu.kim21@gmail.com>
2022-04-09 23:01:54 +0900

    vite... covid...

---
+++ 06_vite_basic/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import './asset/styles/style.scss'
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-lg text-red-500">App</h1>
+      <p className="text-base">hello app!</p>
+    </main>
+  )
+}
+
+export default App
