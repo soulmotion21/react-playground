66e9c616497b4bba4645a542f7624de0180282bb
soulmotion21 <sungkyu.kim21@gmail.com>
2022-04-11 15:33:55 +0900

    vite...covid

--- 06_vite_basic/src/App.tsx
+++ 06_vite_basic/src/App.tsx
@@ -5,7 +5,9 @@ import DatePicker from './components/datepicker/DatePicker'
 function App() {
   return (
     <main className="tw-bg-slate-200">
-      <h1 className="tw-text-lg tw-text-green-500">App</h1>
+      <h1 className="tw-pt-4 tw-pt-2 tw-text-lg tw-text-green-500 tw-shadow-md hover:tw-bg-sky-700">
+        App
+      </h1>
       <p className="tw-text-base">hello app!</p>
       <button className="btn">Submit!</button>
       <DatePicker />
