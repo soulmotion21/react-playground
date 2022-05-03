022fc91ffb634e6d0e74c173d659abf065808b67
soulmotio21 <sungkyu.kim21@gmail.com>
2022-05-03 16:10:15 +0900

    mission5 init

--- 06_vite_basic/src/App.tsx
+++ 06_vite_basic/src/App.tsx
@@ -3,9 +3,65 @@ import './asset/style/style.scss'
 import DatePicker from './components/datepicker/DatePicker'
 
 function App() {
+  React.useEffect(() => {
+    fetch('https://fakestoreapi.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-origin': '*',
+      },
+      body: JSON.stringify({
+        email: 'John@gmail.com',
+        username: 'johnd',
+        password: 'm38rmF$',
+        name: {
+          firstname: 'John',
+          lastname: 'Doe',
+        },
+        address: {
+          city: 'kilcoole',
+          street: '7835 new road',
+          number: 3,
+          zipcode: '12926-3874',
+          geolocation: {
+            lat: '-37.3159',
+            long: '81.1496',
+          },
+        },
+        phone: '1-570-236-7033',
+      }),
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }, [])
+
+  React.useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => console.log('products?sort=desc', json))
+  }, [])
+
+  React.useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=7')
+      .then(res => res.json())
+      .then(json => console.log('products?limit', json))
+  }, [])
+
+  React.useEffect(() => {
+    fetch('https://fakestoreapi.com/products/categories')
+      .then(res => res.json())
+      .then(json => console.log('categories', json))
+  }, [])
+
+  React.useEffect(() => {
+    fetch('https://fakestoreapi.com/products/category/electronics')
+      .then(res => res.json())
+      .then(json => console.log('category/electronics', json))
+  }, [])
+
   return (
     <main className="tw-bg-slate-200">
-      <h1 className="tw-pt-4 tw-pt-2 tw-text-lg tw-text-green-500 tw-shadow-md hover:tw-bg-sky-700">
+      <h1 className="tw-pt-4 tw-text-lg tw-text-green-500 tw-shadow-md hover:tw-bg-sky-700">
         App
       </h1>
       <p className="tw-text-base">hello app!</p>
